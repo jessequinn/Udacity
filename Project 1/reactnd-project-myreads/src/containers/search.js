088544a24c123165f024cd20cb4dc26941478e5b
jessequinn <me@jessequinn.info>
2018-06-29 22:06:28 -0300

    Project 1 complete.

--- Project 1/reactnd-project-myreads/src/containers/search.js
+++ Project 1/reactnd-project-myreads/src/containers/search.js
@@ -1,9 +1,8 @@
-import _ from 'lodash';
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import propTypes from 'prop-types';
-import sortBy from 'sort-by';
-import escapeRegExp from 'escape-string-regexp';
+  import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import propTypes from "prop-types";
+import sortBy from "sort-by";
+import escapeRegExp from "escape-string-regexp";
 
 class Search extends Component {
   static propTypes = {
@@ -14,124 +13,121 @@ class Search extends Component {
     wantedBooks: propTypes.array.isRequired,
     onAddRead: propTypes.func.isRequired,
     readBooks: propTypes.array.isRequired
-  }
+  };
 
   state = {
-    query: ''
-  }
+    query: ""
+  };
 
   updateQuery = query => {
     this.setState({ query: query.trim() });
   };
 
   clearQuery = () => {
-    this.setState({ query: '' });
+    this.setState({ query: "" });
   };
 
   render() {
-    const { books, onAddCurrent, onAddWanted, onAddRead, currentBooks, wantedBooks, readBooks } = this.props;
+    const {
+      books,
+      onAddCurrent,
+      onAddWanted,
+      onAddRead,
+      currentBooks,
+      wantedBooks,
+      readBooks
+    } = this.props;
     const { query } = this.state;
 
     let showingBooks;
     if (query) {
-      const match = new RegExp(escapeRegExp(query), 'i');
+      const match = new RegExp(escapeRegExp(query), "i");
       showingBooks = books.filter(book => match.test(book.title));
     } else {
       showingBooks = [];
     }
 
-    showingBooks.sort(sortBy('title'));
-
-    // split array into chunks
-    // const rows = _.chunk(showingBooks, 4)
-    // console.log(rows)
-    //
-    //
-    // <div className='dropup'>
-    //   <button className='btn btn-secondary dropdown-toggle' type='button' id='dropdownMenuButton' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
-    //     <i className='fas fa-archive'></i>
-    //   </button>
-    //   <div className='dropdown-menu' aria-labelledby='dropdownMenuButton'>
-    //     <button onSelect={() => onAddCurrent(book)} className='dropdown-item'>Currently Reading</button>
-    //     <button className='dropdown-item'>Want to Read</button>
-    //     <button className='dropdown-item'>Read</button>
-    //     <button className='dropdown-item'>None</button>
-    //   </div>
-    // </div>
+    showingBooks.sort(sortBy("title"));
 
     console.log(currentBooks);
     return (
-      <div className='container'>
+      <div className="container">
         <form>
-          <div className='form-group row'>
-            <div className='col-11'>
+          <div className="form-group row">
+            <div className="col-11">
               <input
-                className='form-control'
-                type='text'
-                placeholder='Search for your favourite book!'
+                className="form-control"
+                type="text"
+                placeholder="Search for your favourite book!"
                 value={query}
                 onChange={event => this.updateQuery(event.target.value)}
               />
             </div>
-            <div className='col-1'>
-              <Link to='/' className='btn btn-warning'>
+            <div className="col-1">
+              <Link to="/" className="btn btn-info">
                 Return
               </Link>
             </div>
           </div>
         </form>
-        <div className='row'>
-          <div className='col'>
-            <div className='card-columns'>
+        <div className="row">
+          <div className="col">
+            <div className="card-columns">
               {showingBooks.map(book => (
-                <div className='card' key={book.id}>
+                <div className="card" key={book.id}>
                   <img
-                    className='card-img-top'
+                    className="card-img-top"
                     src={book.imageLinks.smallThumbnail}
-                    alt='Card image cap'
+                    alt="Card image cap"
                   />
-                  <div className='card-body'>
-                    <h5 className='card-title'>{book.title}</h5>
-                    <h6 className='card-subtitle mb-2 text-muted'>
+                  <div className="card-body">
+                    <h5 className="card-title">{book.title}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">
                       {book.subtitle}
                     </h6>
-                    <p className='card-text'>{book.authors.join(', ')}</p>
-                    <button
-                      onClick={() => onAddCurrent(book)}
-                      type='button'
-                      className='btn btn-primary btn-sm'
-                      disabled={
-                        currentBooks.find(e => e.id === book.id)
-                          ? 'disabled'
-                          : null
-                      }
+                    <p className="card-text">{book.authors.join(", ")}</p>
+                    <div
+                      className="btn-group"
+                      role="group"
+                      aria-label="Basic example"
                     >
-                      <i className="fab fa-readme"></i>
-                    </button>
-                    <button
-                      onClick={() => onAddWanted(book)}
-                      type='button'
-                      className='btn btn-warning btn-sm'
-                      disabled={
-                        wantedBooks.find(e => e.id === book.id)
-                          ? 'disabled'
-                          : null
-                      }
-                    >
-                      <i className='fas fa-book' />
-                    </button>
-                    <button
-                      onClick={() => onAddRead(book)}
-                      type='button'
-                      className='btn btn-dark btn-sm'
-                      disabled={
-                        readBooks.find(e => e.id === book.id)
-                          ? 'disabled'
-                          : null
-                      }
-                    >
-                      <i className='fas fa-archive' />
-                    </button>
+                      <button
+                        onClick={() => onAddCurrent(book)}
+                        type="button"
+                        className="btn btn-warning btn-sm"
+                        disabled={
+                          currentBooks.find(e => e.id === book.id)
+                            ? "disabled"
+                            : null
+                        }
+                      >
+                        <i className="fab fa-readme" />
+                      </button>
+                      <button
+                        onClick={() => onAddWanted(book)}
+                        type="button"
+                        className="btn btn-warning btn-sm"
+                        disabled={
+                          wantedBooks.find(e => e.id === book.id)
+                            ? "disabled"
+                            : null
+                        }
+                      >
+                        <i className="fas fa-book" />
+                      </button>
+                      <button
+                        onClick={() => onAddRead(book)}
+                        type="button"
+                        className="btn btn-warning btn-sm"
+                        disabled={
+                          readBooks.find(e => e.id === book.id)
+                            ? "disabled"
+                            : null
+                        }
+                      >
+                        <i className="fas fa-archive" />
+                      </button>
+                    </div>
                   </div>
                 </div>
               ))}
